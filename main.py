4141cdc5fe4e411d484936ee3b698aa6c71f1d2e
LorenzoMasserano <lorenzomasserano94@gmail.com>
2026-06-06 14:58:48 +0200

    init a simple BE with API and SQL DB

--- main.py
+++ main.py
@@ -1,8 +1,40 @@
 from fastapi import FastAPI
-
+from typing import Optional
+from sqlmodel import Field, Session, SQLModel, create_engine, select
 
 app = FastAPI()
 
+class Expense(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    title: str
+    ammount: float
+    category: str
+
+
+engine = create_engine("sqlite:///database.db", connect_args= {"check_same_thread": False})
+
+def init_db():
+    SQLModel.metadata.create_all(engine)
+
+init_db() 
+
+@app.post("/expensive/")
+def add_expensive(expensive: Expense):
+    
+    with Session(engine) as session:
+        session.add(expensive)
+        session.commit()
+        session.refresh(expensive)
+
+    return {"message": "Spesa aggiunta con successo!", "data": expensive}
+
+@app.get("/expensive/")
+def get_all_expensive():
+
+    with Session(engine) as session:
+        statement = select(Expense)
+        result = session.exec(statement).all()
+        return result
 
 @app.get("/")
 def read_root():
